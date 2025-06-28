20e79a33adde46714d36653305fdeae6e278c161
Kittycat <sankhayan2002@gmail.com>
2025-06-28 23:57:07 +0530

    feat: added token fetch

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,104 @@
-import Image from "next/image";
+"use client";
+import { Metaplex } from "@metaplex-foundation/js";
+import { Connection, PublicKey } from "@solana/web3.js";
+import { useEffect } from "react";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+const RAYDIUM_PUBLIC_KEY = "675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8"; //CAMMCzo5YL8w4VFF8KVHrK22GGUsp5VTaW7grrKgrWqK
+const HTTP_URL =
+  "";
+const WSS_URL =
+  "";
+const RAYDIUM = new PublicKey(RAYDIUM_PUBLIC_KEY);
+const INSTRUCTION_NAME = "initialize2";
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+const connection = new Connection(HTTP_URL, {
+  wsEndpoint: WSS_URL,
+});
+
+const metaplex = Metaplex.make(connection);
+async function fetchRaydiumMints(txId: string, connection: Connection) {
+  try {
+    const tx = await connection.getParsedTransaction(txId, {
+      maxSupportedTransactionVersion: 0,
+      commitment: "confirmed",
+    });
+
+    //@ts-ignore
+    const accounts = (tx?.transaction.message.instructions).find(
+      (ix) => ix.programId.toBase58() === RAYDIUM_PUBLIC_KEY
+      //@ts-ignore
+    ).accounts as PublicKey[];
+
+    if (!accounts) {
+      console.log("No accounts found in the transaction.");
+      return;
+    }
+
+    const tokenAIndex = 8;
+    const tokenBIndex = 9;
+
+    const tokenAAccount = accounts[tokenAIndex];
+    const tokenBAccount = accounts[tokenBIndex];
+
+    const metadataAccountA = metaplex
+      .nfts()
+      .pdas()
+      .metadata({ mint: tokenAAccount });
+
+    const metadataAccountInfo = await connection.getAccountInfo(
+      metadataAccountA
+    );
+
+    if (metadataAccountInfo) {
+      const token = await metaplex
+        .nfts()
+        .findByMint({ mintAddress: tokenAAccount });
+      let tokenName = token.name;
+      let tokenSymbol = token.symbol;
+      let tokenLogo = token.json!.image;
+      console.log(tokenName, tokenSymbol, token);
+    }
+
+    const displayData = [
+      { Token: "A", "Account Public Key": metadataAccountInfo },
+      { Token: "B", "Account Public Key": tokenBAccount.toBase58() },
+    ];
+
+    console.log("New LP Found");
+    console.table(metadataAccountInfo);
+  } catch {
+    console.log("Error fetching transaction:", txId);
+    return;
+  }
+}
+async function startConnection(
+  connection: Connection,
+  programAddress: PublicKey,
+  searchInstruction: string
+): Promise<void> {
+  console.log("Monitoring logs for program:", programAddress.toString());
+  connection.onLogs(
+    programAddress,
+    ({ logs, err, signature }) => {
+      if (err) return;
+
+      if (logs && logs.some((log) => log.includes(searchInstruction))) {
+        console.log(
+          "Signature for 'initialize2':",
+          `https://explorer.solana.com/tx/${signature}`
+        );
+        fetchRaydiumMints(signature, connection);
+      }
+    },
+    "finalized"
   );
+}
+export default function Home() {
+  useEffect(() => {
+    async function fetchData() {
+      startConnection(connection, RAYDIUM, INSTRUCTION_NAME);
+    }
+    fetchData();
+  }, []);
+  return <div>Pussycat</div>;
 }
